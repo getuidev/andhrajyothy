b7f8fdbc53905c6322ef46f8149b168159dbb76e
pcuiux <98046751+pcuiux@users.noreply.github.com>
2022-06-25 13:38:16 +0530

    hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
-    // $(".search-icon").click(function () {
-    //     $("#headerSearch").fadeToggle();
-    // });
-    // $("#toggleNav").click(function () {
-    //     $("#megaMenu").slideToggle("slow", function () {
-    //         if ($("#megaMenu").is(":visible")) {
-    //             $("body").addClass("openMenu");
-    //         } else {
-    //             $("body").removeClass("openMenu");
-    //         }
-    //     });
-    // });
-    // $(".listItems li").click(function () {
-    //     if ($(this).hasClass("active")) {
-    //         $(".listItems li").removeClass("active")
-    //     } else {
-    //         $(".listItems li").removeClass("active");
-    //         $(this).addClass("active");
-    //     }
-    // });
+    $(".search-icon").click(function () {
+        $("#headerSearch").fadeToggle();
+    });
+    $("#toggleNav").click(function () {
+        $("#megaMenu").slideToggle("slow", function () {
+            if ($("#megaMenu").is(":visible")) {
+                $("body").addClass("openMenu");
+            } else {
+                $("body").removeClass("openMenu");
+            }
+        });
+    });
+    $(".listItems li").click(function () {
+        if ($(this).hasClass("active")) {
+            $(".listItems li").removeClass("active")
+        } else {
+            $(".listItems li").removeClass("active");
+            $(this).addClass("active");
+        }
+    });
     setTimeout(function () {
         $(window).scroll(function () {
             $(this).scrollTop() > 100 ? $(".scrolltop").fadeIn() : $(".scrolltop")
